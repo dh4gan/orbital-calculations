Simple script to run multiple tests for different orbital speeds Looks like the orbital calculation is giving incorrect argument of periapsis or true anomaly (or both)
Calculations correct if body position.x>0
Incorrect if <0

orbitalelements.py
@@ -110,7 +110,7 @@ class orbitalElements(object):
                 # True Anomaly
                 edotR = eccentricityVector.dot(self.position) 
                 edotR = edotR / (magpos * self.e) 
-
+                print "edotR", edotR, magpos, self.e
                 rdotV = self.velocity.dot(self.position) 
 
                 self.trueanom = np.arccos(edotR) 